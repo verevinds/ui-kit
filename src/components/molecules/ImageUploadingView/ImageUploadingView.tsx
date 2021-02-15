f55aef06b4b19f2569d8d3db8011520bc483ca40
Веревин <d.verevin@vsemayki.ru>
2021-02-15 21:41:00 +0700

    Refactoring

--- src/components/molecules/ImageUploadingView/ImageUploadingView.tsx
+++ src/components/molecules/ImageUploadingView/ImageUploadingView.tsx
@@ -1,5 +1,12 @@
-import React, { SetStateAction, useEffect, useRef, useState } from 'react';
+import React, {
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import ReactImageUploading, {
+  ImageType,
   ImageUploadingPropsType,
 } from 'react-images-uploading';
 import type { ImageListType } from 'react-images-uploading';
@@ -7,22 +14,15 @@ import './imageuploadingview.css';
 import { Button } from '../../atoms/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
-import SwiperCore, {
-  Autoplay,
-  Controller,
-  EffectFade,
-  Navigation,
-  Pagination,
-  Scrollbar,
-} from 'swiper';
+import SwiperCore, { Navigation, Pagination, Virtual } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.min.css';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/components/navigation/navigation.min.css';
-import 'swiper/components/effect-fade/effect-fade.min.css';
 import 'swiper/components/pagination/pagination.min.css';
+import { ExportInterface } from 'react-images-uploading/dist/typings';
 
-SwiperCore.use([EffectFade, Pagination, Navigation]);
+SwiperCore.use([Pagination, Navigation, Virtual]);
 
 export type ImageUploadingViewProps = {
   initialImages: ImageListType;
@@ -46,6 +46,61 @@ export const ImageUploadingView = ({
     if (callback) callback(imageList as never[]);
     setImages(imageList as never[]);
   };
+  const renderItem = useCallback(
+    ({
+      onImageUpdate,
+      onImageRemove,
+    }: Pick<ExportInterface, 'onImageRemove' | 'onImageUpdate'>) => (
+      image: ImageType,
+      index: number,
+    ) => (
+      <SwiperSlide key={image['data_url']} virtualIndex={index}>
+        <div key={image.dataURL} className='upload-view__image-item'>
+          <img
+            src={image['data_url']}
+            alt=''
+            height='150'
+            width='150'
+            className='upload-view__image'
+          />
+          <div className='upload-view__btn-wrapper'>
+            <Button
+              type='button'
+              onClick={() => onImageUpdate(index)}
+              className='upload-view__btn upload-view__btn-upload'
+              variant='outline-primary'
+              icon={<FontAwesomeIcon icon={faPen} />}
+            />
+            <Button
+              type='button'
+              onClick={() => onImageRemove(index)}
+              className='upload-view__btn upload-view__btn-remove'
+              variant='outline-danger'
+              icon={<FontAwesomeIcon icon={faTrash} />}
+            />
+          </div>
+        </div>
+      </SwiperSlide>
+    ),
+    [],
+  );
+
+  const renderSwiper: ImageUploadingPropsType['children'] = useCallback(
+    ({ imageList, onImageUpdate, onImageRemove }: ExportInterface) => (
+      <Swiper
+        spaceBetween={10}
+        slidesPerView='auto'
+        watchOverflow={true}
+        navigation
+        pagination={{ clickable: true }}
+        className='upload-view'
+        virtual
+      >
+        {imageList.map(renderItem({ onImageUpdate, onImageRemove }))}
+      </Swiper>
+    ),
+    [renderItem],
+  );
 
   return (
     <ReactImageUploading
@@ -56,46 +111,7 @@ export const ImageUploadingView = ({
       acceptType={acceptType}
       dataURLKey='data_url'
     >
-      {({ imageList, onImageUpdate, onImageRemove }) => (
-        <Swiper
-          spaceBetween={10}
-          slidesPerView='auto'
-          watchOverflow={true}
-          navigation
-          pagination={{ clickable: true }}
-          className='upload-view'
-        >
-          {imageList.map((image, index) => (
-            <SwiperSlide>
-              <div key={image.dataURL} className='upload-view__image-item'>
-                <img
-                  src={image['data_url']}
-                  alt=''
-                  height='150'
-                  width='150'
-                  className='upload-view__image'
-                />
-                <div className='upload-view__btn-wrapper'>
-                  <Button
-                    type='button'
-                    onClick={() => onImageUpdate(index)}
-                    className='upload-view__btn upload-view__btn-upload'
-                    variant='outline-primary'
-                    icon={<FontAwesomeIcon icon={faPen} />}
-                  />
-                  <Button
-                    type='button'
-                    onClick={() => onImageRemove(index)}
-                    className='upload-view__btn upload-view__btn-remove'
-                    variant='outline-danger'
-                    icon={<FontAwesomeIcon icon={faTrash} />}
-                  />
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )}
+      {renderSwiper}
     </ReactImageUploading>
   );
 };
