e14d17cf218bb4b6d595cf5e1c672d009b9f0b98
Веревин Денис <d.verevin@vsemayki.ru>
2021-02-18 15:16:54 +0700

    Add export checkbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 export { default as Button } from '../src/components/atoms/Button/Button';
 export { default as Input } from '../src/components/atoms/Input';
+export { default as Checkbox } from '../src/components/atoms/Checkbox';
 export { default as ImageUploadingAdd } from '../src/components/atoms/ImageUploadingAdd';
 export { default as ImageUploadingView } from '../src/components/molecules/ImageUploadingView/ImageUploadingView';
